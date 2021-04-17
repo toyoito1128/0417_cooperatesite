1b320c6b686344fbb2f57d8da689ae08894817cf
toyohiro <toyoito1128@gmail.com>
2021-04-17 15:36:05 +0900

    0417_1

---
+++ main.js
@@ -0,0 +1,13 @@
+// const menu = document.getElementById("menu");
+
+menu.addEventListener("click", function () {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("open");
+  const drawer = document.getElementById("drawer");
+  drawer.classList.toggle("in");
+});
+
+
+
+
+
